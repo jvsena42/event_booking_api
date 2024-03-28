b0fadca73e3eba4c9be2c0fcde60fa641992e341
jvsena42 <jvsena16@gmail.com>
2024-03-28 19:56:52 -0300

    feat: init db

--- db/db.go
+++ db/db.go
@@ -9,12 +9,34 @@ import (
 var DB *sql.DB
 
 func InitDB() {
-	DB, err := sql.Open("sqlite3", "api.db")
+	var err error
+	DB, err = sql.Open("sqlite3", "api.db")
 
 	if err != nil {
-		panic("Database error")
+		panic("ERROR Could not connect to database")
 	}
 
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
+
+	createTables()
+}
+
+func createTables() {
+	createEventsTable := `
+	CREATE TABLE IF NOT EXISTS events (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		location TEXT NOT NULL,
+		dateTime DATETIME NOT NULL,
+		user_id INTEGER
+	)
+	`
+
+	_, err := DB.Exec(createEventsTable)
+
+	if err != nil {
+		panic(err)
+	}
 }
--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"com.go/event_booking/db"
 	"com.go/event_booking/model"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
